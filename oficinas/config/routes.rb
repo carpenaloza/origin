0b3afe6f7f9b5427dddbfca1a11ea197113359b6
carpenaloza <car.penaloza@hotmail.es>
2021-08-23 13:42:37 -0400

    prueba

--- oficinas/config/routes.rb
+++ oficinas/config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   post 'oficinas/guardar', to: 'oficinas#guardar'
 
   # rutas inquilinos
-  get 'inquilinos'            , to: 'inquilinos#listar'
-  get 'inquilinos/crear'      , to: 'inquilinos#crear'
-  get 'inquilinos/:id'        , to: 'inquilinos#mostrar'
-  get 'inquilinos/:id/editar' , to: 'inquilinos#editar'
+  get 'inquilinos'            , to: 'inquilinos#listar', as: 'inquilinos'
+  get 'inquilinos/crear'      , to: 'inquilinos#crear', as: 'nuevo_inquilino'
+  get 'inquilinos/:id'        , to: 'inquilinos#mostrar', as: 'inquilino'
+  get 'inquilinos/:id/editar' , to: 'inquilinos#editar', as: 'editar_inquilino'
 
   post    'inquilinos/crear',   to: 'inquilinos#guardar'
   put     'inquilinos/:id',     to: 'inquilinos#actualizar'
